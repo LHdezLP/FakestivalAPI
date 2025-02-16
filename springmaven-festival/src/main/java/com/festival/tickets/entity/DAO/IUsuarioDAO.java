6252fd9291714f75655336ea22d7084798381020
LHdezLP <145345056+LHdezLP@users.noreply.github.com>
2025-02-16 13:34:18 +0000

    Delete springmaven-festival directory

--- springmaven-festival/src/main/java/com/festival/tickets/entity/DAO/IUsuarioDAO.java
+++
@@ -1,8 +0,0 @@
-package com.festival.tickets.entity.DAO;
-
-import com.festival.tickets.entity.models.Usuario;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IUsuarioDAO extends CrudRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
-}
